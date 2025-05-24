d0152f2adb6c0aa8193a03931327418f3144b23e
frankedierg <frankediergb@gmail.com>
2025-05-24 14:25:02 -0500

    frontend actualizado-minty-color-formulario-login y cards

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,13 +1,15 @@
 import { importProvidersFrom } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
-import { FormsModule } from '@angular/forms';  // 👈 Este es el módulo que activa ngModel
+import { FormsModule } from '@angular/forms';
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
+
 
 export const appConfig = {
   providers: [
     provideRouter(routes),
     importProvidersFrom(BrowserModule, FormsModule, HttpClientModule)
+    
   ]
 };
